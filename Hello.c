598fbc9afcad60251ed50c2cc4632ea04f8fa943
Ricardo Rossini <ricardo.rossini.dev@outlook.com>
2024-04-30 18:50:05 -0300

    first commit

---
+++ Hello.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+    void main(){
+        printf("Hello World");
+
+        getch();
+
+        // comment!
+
+        /*
+        multiple lines comment!
+        */
+    }
